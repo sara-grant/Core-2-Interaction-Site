8e240630421d193ee90de6a3a3cca47ac522e728
sara-grant <grans670@newschool.edu>
2025-03-06 10:00:43 -0500

    penny presser udate

--- studio/workshops/penny-presser/penny-presser/scripts.js
+++ studio/workshops/penny-presser/penny-presser/scripts.js
@@ -2,6 +2,8 @@
 
 var pressButton = document.querySelector('button[name="press"]');
 
+var studentSelect = document.querySelector('select[name="student"]');
+
 var pennySelect = document.querySelector('select[name="penny"]');
 
 var pennyImage = document.querySelector('.penny');
@@ -12,10 +14,18 @@ pressButton.addEventListener('click', () => {
 
     console.log(pennySelect.value);
 
-    pennyImage.src= 'img/'+ pennySelect.value;
+    // pennyImage.src= 'img/'+ pennySelect.value;
+
+    pennyImage.src='https://' + studentSelect.value + '.interactive.rodeo/studio/workshops/penny-presser/img/' + pennySelect.value;
 
     console.log(pennyImage);
 });
 
+
+//value
+console.log(studentSelect.value);
+console.log(pennySelect.value);
+
 // console
+console.log(pennyImage);
 console.log(pressButton);
